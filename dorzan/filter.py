39ba392d77835adb720a65de35ff7c55cad2354f
Rodrik Vartoomian <rodrikwork@gmail.com>
2023-04-26 15:47:45 +0330

    Add markdown formatting for error messages

--- dorzan/filter.py
+++ dorzan/filter.py
@@ -142,6 +142,6 @@ if __name__ == "__main__":
                 break
         except Exception as e:
             if bot:
-                bot.broadcast_admins(f"Error in updating hosts ({i+1}/3): `{e}`")
+                bot.broadcast_admins(f"Error in updating hosts ({i+1}/3): ```{e}```")
 
         time.sleep(5)
